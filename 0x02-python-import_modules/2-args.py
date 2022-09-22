bf105af59dac5be3ff69504d7471e56e538306c4
adesanya <adesanyaopeyemi06@gmail.com>
2022-09-22 11:58:17 +0000

    welcome back python 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     i = len(sys.argv) - 1
 
-     if i == 0:
-         print("{} arguments.".format(i))
-      elif i == 1:
-          print("{} argument:".format(i))
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
         print("{} arguments:".format(i))
 
